45b80be74523481164817b674d2fbc6e82bfb5fc
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-03-23 10:33:48 +0700

    Add project files.

---
+++ belajar insertion sort/belajar insertion sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];						// Membuat Array dengan panjang data 20
+int n;								// Membuat Variabel inputan n
+
+void input() {				// Procedure Input
+	while (true)
+	{
+		cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
+		cin >> n;									  // memanggil variabel inputan n 
+
+		if (n <= 20) {								  // Membuat Kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 Elemen. \n"; // Menampilkan Pesan jika data lebih dari 20
+		}
+	}
+	cout << endl;									    // Membuat jarak per baris program
+	cout << "======================" << endl;			// Membuat tampilan susunan data element array
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++)	{						// Menggunakan perulangan for untuk menyimpan data pada array
+}
