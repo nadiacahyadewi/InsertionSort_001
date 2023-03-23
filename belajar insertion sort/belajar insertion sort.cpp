6549404585387d05834d2d7e6b2c88394b9a2e15
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-03-23 10:49:45 +0700

    Membuat procedur input

--- belajar insertion sort/belajar insertion sort.cpp
+++ belajar insertion sort/belajar insertion sort.cpp
@@ -23,5 +23,9 @@ void input() {				// Procedure Input
 	cout << "Masukkan Element Array" << endl;
 	cout << "======================" << endl;
 
-	for (int i = 0; i < n; i++)	{						// Menggunakan perulangan for untuk menyimpan data pada array
+	for (int i = 0; i < n; i++)							// Menggunakan perulangan for untuk menyimpan data pada array
+	{
+		cout << "Data ke-" << (i + 1) << ": ";			// Memasukkan atau menginputkan nilai data n
+		cin >> arr[i];									// Menyimpan nilai data n kedalam array arr
+	}
 }
